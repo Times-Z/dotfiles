b7692b89412131f628a13901b8c12870955f223c
timesz <crashzeus@protonmail.com>
2023-02-20 17:48:50 +0100

    fix: badscript

--- polybar/scripts/battery_notify.sh
+++ polybar/scripts/battery_notify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### VARIABLES
+
 LOW_BAT=20           # lesser than this is considered low battery
 ICON_PATH="/usr/share/icons/Tela-circle-black-dark/symbolic/status/battery-empty-symbolic.svg"
 ALREADY_NOTIFY=false
@@ -10,22 +12,19 @@ ALREADY_NOTIFY=false
 #
 BAT_PATH=/sys/class/power_supply/BAT0
 
-while true
-do
-	BAT_LEVEL=$(cat "$BAT_PATH/capacity")
+BAT_LEVEL=$(cat "$BAT_PATH/capacity")
 
-	if [ $BAT_LEVEL -lt $LOW_BAT ]
+if [ $BAT_LEVEL -lt $LOW_BAT ]
+then
+	if [ $ALREADY_NOTIFY = false ]
 	then
-		if [ $ALREADY_NOTIFY = false ]
-		then
-			ALREADY_NOTIFY=true
-			dunstify --appname="battery" "Battery low" --urgency="critical" -i $ICON_PATH
-			paplay $NOTIFICATION_SOUND 
-		fi
-	else
-		if [ $ALREADY_NOTIFY = true ]
-		then
-			ALREADY_NOTIFY=false
-		fi
+		ALREADY_NOTIFY=true
+		dunstify --appname="battery" "Battery low" --urgency="critical" -i $ICON_PATH
+		paplay $NOTIFICATION_SOUND 
 	fi
-done
+else
+	if [ $ALREADY_NOTIFY = true ]
+	then
+		ALREADY_NOTIFY=false
+	fi
+fi
