e79986c638c8ad9811cdc9708d288b65632f3d3e
timesz <crashzeus@protonmail.com>
2023-03-20 22:56:54 +0100

    feat: install script

--- install.sh
+++ install.sh
@@ -1,61 +1,101 @@
 #!/bin/bash
+declare -A PACKAGES_MANAGER
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 USER=$(whoami)
+PACKAGES=(
+  "bspwm"
+  "dunst"
+  "gpicview"
+  "kitty"
+  "neofetch"
+  "neovim"
+  "picom"
+  "polybar"
+  "ranger"
+  "rofi"
+  "sxhkd"
+)
+PACKAGES_MANAGER=(
+  [yum]="/etc/redhat-release;yum install"
+  [pacman]="/etc/arch-release;pacman -Syy"
+  [debian]="/etc/debian_version;apt install"
+)
 
 function backup_config() {
-    echo "=> Backup $1 configuration"
-    cp -R $1 $1.bck
+  echo "=> Backup $1 configuration"
+  cp -R $1 $1.bck
 }
 
-function install() {
+function get_package_manager(){
+  for os in ${!PACKAGES_MANAGER[@]}
+  do
+    if [[ -f $(echo ${PACKAGES_MANAGER[$os]} | cut -d ";" -f1) ]]
+    then
+      PACKAGE_MANAGER=$os
+      PACKAGE_MANAGER_INSTALL_PHRASE=$(echo ${PACKAGES_MANAGER[$os]} | cut -d ";" -f2)
+      return 0
+    fi
+  done
+}
+
+function config() {
     backup_config $3
     echo " => Installing $2 configuration"
-    case $1 in
-        
-        "--dir")
-            mkdir -p $3
-            cp -R $SCRIPT_DIR/$2/* $3/
-            echo "dir mode"
-        ;;
-        
-        "--file")
-            cp $SCRIPT_DIR/$2 $3
-            echo "file mode"
-        ;;
-        
-        *)
-            echo "Bad option"
-            exit 1
-        ;;
+    case $1 in       
+      "--dir")
+        mkdir -p $3
+        cp -R $SCRIPT_DIR/$2/* $3/
+      ;;
+      "--file")
+        cp $SCRIPT_DIR/$2 $3
+      ;;
+      *)
+        echo "Bad option"
+        exit 1
+      ;;
     esac
-    
-    chown -R $USER $2
-    echo " => Done"
+  chown -R $USER $2
+  echo " => Done"
 }
 
+function install_package() {
+  packages=${PACKAGES[@]}
+  get_package_manager
+  read -p "Do you want to insall ($packages) with $PACKAGE_MANAGER ? [y/n] " -n 1 -r
+  echo
+  if [[ ! "$REPLY" =~ ^([Yy]| )$ ]]
+  then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 
+  fi
+  $(sudo $PACKAGE_MANAGER_INSTALL_PHRASE $packages)
+}
 
-echo "----------------------"
-echo "Starting configuration"
+function install_configuration() {
+  echo "----------------------"
+  echo "Starting configuration"
+  config --dir bspwm ~/.config/bspwm
+  config --dir dunst ~/.config/dunst
+  config --dir gpicview ~/.config/gpicview
+  config --dir kitty ~/.config/kitty
+  config --dir neofetch ~/.config/neofetch
+  config --dir nvim ~/.config/nvim
+  config --dir picom ~/.config/picom
+  config --dir polybar ~/.config/polybar
+  config --dir ranger ~/.config/ranger
+  config --dir rofi ~/.config/rofi
+  config --dir spotify-tui ~/.config/spotify-tui
+  config --dir spotifyd ~/.config/spotifyd
+  config --dir sxhkd ~/.config/sxhkd
 
-install --dir bspwm ~/.config/bspwm
-install --dir dunst ~/.config/dunst
-install --dir gpicview ~/.config/gpicview
-install --dir kitty ~/.config/kitty
-install --dir neofetch ~/.config/neofetch
-install --dir nvim ~/.config/nvim
-install --dir picom ~/.config/picom
-install --dir polybar ~/.config/polybar
-install --dir ranger ~/.config/ranger
-install --dir rofi ~/.config/rofi
-install --dir spotify-tui ~/.config/spotify-tui
-install --dir spotifyd ~/.config/spotifyd
-install --dir sxhkd ~/.config/sxhkd
+  config --file .zshrc ~/.zshrc
+  config --file .xinitrc ~/.xinitrc
+  config --file .zprofile ~/.zprofile
+  config --file betterlockscreenrc ~/.config/betterlockscreenrc
 
-install --file .zshrc ~/.zshrc
-install --file .xinitrc ~/.xinitrc
-install --file .zprofile ~/.zprofile
-install --file betterlockscreenrc ~/.config/betterlockscreenrc
+  echo "All done"
+  echo "----------------------"
+}
 
-echo "All done"
-echo "----------------------"
+install_configuration
+install_package
