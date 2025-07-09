9b29216bd78ee1a9a6d735ca8189c54b0b3341b6
TimesZ <jonas.bertin49@gmail.com>
2025-07-09 23:08:06 +0200

    fix: no-confirm needed

--- install.sh
+++ install.sh
@@ -31,14 +31,14 @@ fi
 
 if [ -f pacman.txt ]; then
     echo "[INFO] Installing pacman packages from pacman.txt..."
-    sudo pacman -Syu --needed $(grep -vE '^[ \t]*#' pacman.txt | xargs)
+    sudo pacman -Syu --no-confirm --needed $(grep -vE '^[ \t]*#' pacman.txt | xargs)
 else
     echo "[WARN] pacman.txt not found, skipping pacman packages."
 fi
 
 if [ -f yay.txt ]; then
     echo "[INFO] Installing AUR packages from yay.txt..."
-    yay -S --needed $(grep -vE '^[ \t]*#' yay.txt | xargs)
+    yay -S --needed --no-confirm $(grep -vE '^[ \t]*#' yay.txt | xargs)
 else
     echo "[WARN] yay.txt not found, skipping AUR packages."
 fi
